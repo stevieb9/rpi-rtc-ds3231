e3a4aa0fab6a0c28bda437593f9c32b33db74f7a
Steve Bertrand <steveb@cpan.org>
2018-05-24 11:07:28 -0700

    work on cleaning up header file layout

--- rtc.h
+++ rtc.h
@@ -3,21 +3,32 @@
 #endif
 
 void  _establishI2C (int fd);
-int getMeridien (int fd);
-void setMeridien (int fd, int value);
-int getMilitary (int fd);
-void setMilitary (int fd, int value);
-int getHour (int fd);
-void setHour (int fd, int value);
-int getMinutes (int fd);
-void setMinutes (int fd, int value);
+
 int getFh ();
+
+int getSeconds (int fd);
+int getMinutes (int fd);
+int getHour (int fd);
+
+int getMeridien (int fd);
+int getMilitary (int fd);
+
+void setSeconds int fd, int value);
+void setMinutes (int fd, int value);
+void setHour (int fd, int value);
+
+void setMeridien (int fd, int value);
+void setMilitary (int fd, int value);
+
 void disableRegisterBit (int fd, int reg, int bit);
 void enableRegisterBit (int fd, int reg, int bit);
+
 int getRegister (int fd, int reg);
 int getRegisterBit (int fd, int reg, int bit);
 int getRegisterBits (int fd, int reg, int msb, int lsb);
+
 int setRegister(int fd, int reg, int value, char* name);
 int setRegisterBits(int fd, int reg, int lsb, int nbits, int value, char* name);
+
 int bcd2dec(int num);
 int dec2bcd(int num);
